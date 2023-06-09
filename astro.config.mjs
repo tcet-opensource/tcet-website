e529ccaf64c95874b186f28698bba70b31e2c8fc
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-06-09 15:12:36 +0530

    FEAT. setup done

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind(), sitemap(), robotsTxt(), image()],
+  base: "/",
+  site: "https://tcetmumbai.in/",
+  vite: {
+    ssr: {
+      noExternal: [/^swiper\/*/,/^react-fast-marquee\/*/,/^usehooks-ts\/*/,/^react-countup\/*/ ]
+    }
+  },
+});
