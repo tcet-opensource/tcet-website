8d130572b8cc869d46f9748b7bb688df3a78e7bd
Horror26 <1032210438@tcetmumbai.in>
2023-06-12 12:04:40 +0530

    [Feature] certified-by-section

---
+++ src/components/Home-page/certified-by-section/CertifiedSection.tsx
@@ -0,0 +1,19 @@
+
+interface CertiProps {
+  img: any;
+  text:string;
+}
+const certifiedSection: React.FC<CertiProps> = ({ img,text }) => {
+  return (
+    <div className=' w-80 h-36 p-2 gap-4 flex flex-col  items-center '>
+      <div>
+        <img src={img} alt="test" /> 
+      </div>
+      <div className=' w-56 h-15 font-normal text-sm leading-4 text-center text-[#475467]'>
+      <p>{text}</p>
+      </div>
+    </div>
+  )
+}
+
+export default certifiedSection
