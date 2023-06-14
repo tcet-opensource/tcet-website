cd8f303f7198311337ac9fcaa40579159a704895
Horror26 <1032210438@tcetmumbai.in>
2023-06-14 16:36:52 +0530

    scrollbar hover effect edited

--- src/components/HomePage/certified-by-section/ScrollBar.tsx
+++ src/components/HomePage/certified-by-section/ScrollBar.tsx
@@ -7,7 +7,7 @@ const ScrollBar: React.FC<CertiProps> = ({ img, text }) => {
   return (
     <div>
       <a href="/">
-        <div className=' hover:bg-slate-200 w-max h-14  p-3 gap-2 items-center flex flex-row border border-solid border-gray-300 shadow-sm rounded-lg '>
+        <div className=' hover:bg-slate-100 w-max h-14  p-3 gap-2 items-center flex flex-row border border-solid border-gray-300 shadow-sm rounded-lg '>
           <div>
             <img src={`/HomePage/certified-by-section/${img}`} alt="test" />
           </div>
