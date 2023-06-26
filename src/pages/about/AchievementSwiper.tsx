b83b52fd584048fd05779c3ffe19f4303c976cc3
Prachi Vaishya <vaishyaprachi14@gmail.com>
2023-06-26 22:03:01 +0530

    [REFACTOR] swiper was modified

--- src/pages/about/AchievementSwiper.tsx
+++ src/pages/about/AchievementSwiper.tsx
@@ -12,13 +12,15 @@ const data = [
 
 export default function AchievementSwiper() {
   return (
-    <Swiper
+    <div className="relative p-5 md:p-4">
+      <Swiper
       navigation={{
         prevEl: ".swiper-prev",
         nextEl: ".swiper-next",
       }}
       modules={[Navigation]}
-      className="mySwiper"
+      className="mySwiper w-full"
+      spaceBetween={10}
       breakpoints={{
         420: {
           slidesPerView: 2,
@@ -27,28 +29,31 @@ export default function AchievementSwiper() {
           slidesPerView: 3,
         },
         768: {
-          slidesPerView: 4,
+          slidesPerView: 3,
         },
         1024: {
-          slidesPerView: 4,
+          slidesPerView: 3,
         },
         1280: {
           slidesPerView: 4,
         },
       }}
     >
+      <div>
       {data.map((d, i) => (
-        <SwiperSlide key={i} className="relative p-2 px-2 w-full text-center">
-          <p>{d.text}</p>
+        <SwiperSlide key={i} className="relative p-5 w-full text-center justify-between"> 
+          <p >{d.text}</p>
         </SwiperSlide>
       ))}
-      <div className="swiper-prev m--5 flex items-center justify-center shadow-md shadow-[0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06);] absolute z-10 top-1/2 left-2 transform -translate-y-1/2 bg-white w-8 h-8 rounded-full">
-          <img src="/misc/chevron-left.svg" alt="prev-image-btn" />
-          {/* {`<` font-bold text-2xl text-sky-700} */}
       </div>
-      <div className="swiper-next flex items-center justify-center shadow-md shadow-[0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06);] absolute z-10 top-1/2 right-2 transform -translate-y-1/2 bg-white w-8 h-8 rounded-full">
-          <img src="/misc/chevron-right.svg" alt="next-image-btn" />
-      </div>
+      
     </Swiper>
+    <div className="swiper-prev flex items-center justify-center shadow-md shadow-[0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06);] absolute left-2 bg-white z-10 top-1/2 w-8 h-8 rounded-full">
+    <img src="/misc/chevron-left.svg" alt="prev-image-btn" />
+    </div>
+    <div className="swiper-next absolute flex items-center justify-center shadw-md shadow-[0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06);] absolute right-2 bg-white z-10 top-1/2 w-8 h-8 rounded-full">
+        <img src="/misc/chevron-right.svg" alt="next-image-btn" />
+    </div>
+    </div>
   );
 }
