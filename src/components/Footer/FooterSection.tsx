b2f4cd3b61b809742d82f4a301af80dac6e3edb3
Aakshun Chatla <aakshunrc14@gmail.com>
2023-06-27 23:42:11 +0530

    udated footer file

--- src/components/Footer/FooterSection.tsx
+++ src/components/Footer/FooterSection.tsx
@@ -10,8 +10,8 @@ interface FooterProps {
 export const FooterSection: React.FC<FooterProps>= ({title,links}) => {
     return (
     <div>
-        <h2 className="mb-4 text-sm font-semibold font-small" style={{color:'#98A2B3'}}>{title}</h2>
-        <ul className="font-medium" style={{color:'#667085'}}>
+        <h2 className="mb-4 text-sm font-semibold font-small text-[#98A2B3]">{title}</h2>
+        <ul className="font-medium text-['#667085']">
             {
                 links.map((text) => (
                     <li className="mb-4">
