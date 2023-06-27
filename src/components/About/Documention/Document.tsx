54dbf4f620d89c75f4308c90281b02ea8dcb25c5
Horror26 <1032210438@tcetmumbai.in>
2023-06-27 22:58:00 +0530

    icon width and optimization of code

--- src/components/About/Documention/Document.tsx
+++ src/components/About/Documention/Document.tsx
@@ -9,9 +9,9 @@ export const Documentation = ({ title, url, img }: DocumentProps) => {
   return (
 
     <a href={url}>
-      <div className=' p-3 gap-1 items-center justify-center flex flex-row  border border-solid shadow-sm rounded-lg hover:cursor-pointer '>
+      <div className=' p-3 gap-2 items-center justify-center flex flex-row  border border-solid shadow-sm rounded-lg hover:cursor-pointer '>
         <div>
-          <img src={img} alt="test" /> </div>
+          <img src={img} className="min-w-max" alt="document icon" /> </div>
         <div className='font-normal  text-base leading-normal '>
           <p>{title}</p>
         </div>
