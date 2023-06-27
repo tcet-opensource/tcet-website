c92e3cf613521000c29dbf4c7358da2ca0fa192e
Aakshun Chatla <aakshunrc14@gmail.com>
2023-06-27 23:59:26 +0530

    removed quotes

--- src/components/Footer/FooterSection.tsx
+++ src/components/Footer/FooterSection.tsx
@@ -11,7 +11,7 @@ export const FooterSection: React.FC<FooterProps>= ({title,links}) => {
     return (
     <div>
         <h2 className="mb-4 text-sm font-semibold font-small text-[#98A2B3]">{title}</h2>
-        <ul className="font-medium text-['#667085']">
+        <ul className="font-medium text-[#667085]">
             {
                 links.map((text) => (
                     <li className="mb-4">
