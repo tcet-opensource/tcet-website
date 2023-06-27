55200839517c7d715f96fd6f5fa55650f4387e34
Horror26 <1032210438@tcetmumbai.in>
2023-06-27 10:58:27 +0530

    revert back with given changes

--- src/components/About/Documention/data.js
+++ src/components/About/Documention/data.js
@@ -1,305 +1,84 @@
-// export const data = [
-//   {
-//     title: "NIRF DATA",
-//     color: [
-//       {
-//         bgColor: "#FFFCF5",
-//         textColor: "#DC6803",
-//         borderColor: "#FEC84B",
-//         hoverColor: "#fbf2dd",
-//         files: [
-//           {
-//             title: "NIRF 2016",
-//             url: "https://www.tcetmumbai.in/NIRF_2016.html",
-//           },
-//           {
-//             title: "NIRF 2017",
-//             url: "https://www.tcetmumbai.in/NIRF_2017.html",
-//           },
-//           {
-//             title: "NIRF 2018",
-//             url: "https://www.tcetmumbai.in/NIRF_2018.html",
-//           },
-//           {
-//             title: "NIRF 2019",
-//             url: "https://www.tcetmumbai.in/NIRF_2019.html",
-//           },
-//           {
-//             title: "NIRF 2020",
-//             url: "https://www.tcetmumbai.in/NIRF_2020.html",
-//           },
-//           {
-//             title: "NIRF 2021",
-
-//             url: "https://www.tcetmumbai.in/NIRF_2021.html",
-//           },
-//         ],
-//       },
-//     ],
-//   },
-
-//   {
-//     title: "Institutional Activities",
-//     color: [
-//       {
-//         bgColor: "#ECFDF3",
-//         textColor: "#039855",
-//         borderColor: "#6CE9A6",
-//         hoverColor: "#cee8d8",
-
-//         files: [
-//           {
-//             title: "Academic Leadership",
-
-//             url: "https://www.tcetmumbai.in/Notice/Academic%20Leadership%20-%202017.pdf",
-//           },
-//           {
-//             title: "Degree Certificate Distribution Ceremony",
-
-//             url: "https://www.tcetmumbai.in/Notice/DCDCReport2019.pdf",
-//           },
-//           {
-//             title: "Multicon-W",
-
-//             url: "https://multiconw.co.in/",
-//           },
-//           {
-//             title: "Zephyr",
-//             url: "https://www.tcetmumbai.in/Notice/Zephyr_Report.pdf",
-//           },
-//           {
-//             title: "मातृभाषा दिवस",
-//             url: "https://www.tcetmumbai.in/Notice/MatribhashaDivasReport.pdf",
-//           },
-//           {
-//             title: "Women's day",
-//             url: "https://www.tcetmumbai.in/Notice/Women's%20Day%20Report.pdf",
-//           },
-//           {
-//             title: "Teacher's Day",
-//             url: "https://www.tcetmumbai.in/Notice/Teachers'%20Day%20report.pdf",
-//           },
-//           {
-//             title: "Alumni",
-//             url: "/",
-//           },
-//           {
-//             title: "GUSTO-Online Cultural Event",
-//             url: "https://www.tcetmumbai.in/Notice/GUSTO.pdf",
-//           },
-//           {
-//             title: "Sojourn",
-//             url: "/",
-//           },
-//           {
-//             title: "STTP",
-//             url: "https://www.tcetmumbai.in/Notice/STTP%20Report.pdf",
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   {
-//     title: "Educational Survey",
-//     color: [
-//       {
-//         bgColor: "#F5FAFF",
-//         textColor: "#2E90FA",
-//         borderColor: "#2E90FA",
-//         hoverColor: "#deecfa",
-//         files: [
-//           {
-//             title: "SUSTAINABILITY OF ENGINEERING EDUCATION",
-//             url: "https://www.tcetmumbai.in/Notice/SEE.pdf",
-//           },
-//           {
-//             title: "Student Satisfacation Survey",
-//             url: "https://www.tcetmumbai.in/Notice/SSSurvey.pdf",
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ];
-
-export const data = [
+export const NIRFData = [
   {
-    title: "NIRF DATA",
-    bgColor: "#FFFCF5",
-    textColor: "#DC6803",
-    borderColor: "#FEC84B",
-    hoverColor: "#fbf2dd",
-    files: [
-      {
-        title: "NIRF 2016",
-        url: "https://www.tcetmumbai.in/NIRF_2016.html",
-      },
-      {
-        title: "NIRF 2017",
-        url: "https://www.tcetmumbai.in/NIRF_2017.html",
-      },
-      {
-        title: "NIRF 2018",
-        url: "https://www.tcetmumbai.in/NIRF_2018.html",
-      },
-      {
-        title: "NIRF 2019",
-        url: "https://www.tcetmumbai.in/NIRF_2019.html",
-      },
-      {
-        title: "NIRF 2020",
-        url: "https://www.tcetmumbai.in/NIRF_2020.html",
-      },
-      {
-        title: "NIRF 2021",
-
-        url: "https://www.tcetmumbai.in/NIRF_2021.html",
-      },
-    ],
+      "year":"NIRF 2016",
+      "url":"https://www.tcetmumbai.in/NIRF_2016.html"
   },
   {
-    title: "Institutional Activities",
-    bgColor: "#ECFDF3",
-    textColor: "#039855",
-    borderColor: "#6CE9A6",
-    hoverColor: "#cee8d8",
-    files: [
-      {
-        title: "Academic Leadership",
-
-        url: "https://www.tcetmumbai.in/Notice/Academic%20Leadership%20-%202017.pdf",
-      },
-      {
-        title: "Degree Certificate Distribution Ceremony",
-
-        url: "https://www.tcetmumbai.in/Notice/DCDCReport2019.pdf",
-      },
-      {
-        title: "Multicon-W",
-
-        url: "https://multiconw.co.in/",
-      },
-      {
-        title: "Zephyr",
-        url: "https://www.tcetmumbai.in/Notice/Zephyr_Report.pdf",
-      },
-      {
-        title: "मातृभाषा दिवस",
-        url: "https://www.tcetmumbai.in/Notice/MatribhashaDivasReport.pdf",
-      },
-      {
-        title: "Women's day",
-        url: "https://www.tcetmumbai.in/Notice/Women's%20Day%20Report.pdf",
-      },
-      {
-        title: "Teacher's Day",
-        url: "https://www.tcetmumbai.in/Notice/Teachers'%20Day%20report.pdf",
-      },
-      {
-        title: "Alumni",
-        url: "/",
-      },
-      {
-        title: "GUSTO-Online Cultural Event",
-        url: "https://www.tcetmumbai.in/Notice/GUSTO.pdf",
-      },
-      {
-        title: "Sojourn",
-        url: "/",
-      },
-      {
-        title: "STTP",
-        url: "https://www.tcetmumbai.in/Notice/STTP%20Report.pdf",
-      },
-    ],
+      "year":"NIRF 2017",
+      "url":"https://www.tcetmumbai.in/NIRF_2017.html"
   },
   {
-    title: "Educational Survey",
-    bgColor: "#F5FAFF",
-    textColor: "#2E90FA",
-    borderColor: "#2E90FA",
-    hoverColor: "#deecfa",
-    files: [
-      {
-        title: "SUSTAINABILITY OF ENGINEERING EDUCATION",
-        url: "https://www.tcetmumbai.in/Notice/SEE.pdf",
-      },
-      {
-        title: "Student Satisfacation Survey",
-        url: "https://www.tcetmumbai.in/Notice/SSSurvey.pdf",
-      },
-    ],
+      "year":"NIRF 2018",
+      "url":"https://www.tcetmumbai.in/NIRF_2018.html"
   },
   {
-    title: "others",
-    color: "#475467",
-    files: [
-      {
-        title: "Mandatory Disclosure",
-        url: "https://tcetmumbai.in/NEW%20FORMAT%20-%20Mandatory-Disclosure-for-2022-23-UPDATED-17.01.23%20(3).pdf",
-      },
-    ],
+      "year":"NIRF 2019",
+      "url":"https://www.tcetmumbai.in/NIRF_2019.html"
+  },
+  {
+      "year":"NIRF 2020",
+      "url":"https://www.tcetmumbai.in/NIRF_2020.html"
+  },
+  {
+      "year":"NIRF 2016",
+      "url":"https://www.tcetmumbai.in/NIRF_2021.html"
   },
 ];
 
-<<<<<<< HEAD
 export const InstitutionalActivitesData = [
-    {
-        "title":"Academic Leadership",
-        "url":'https://www.tcetmumbai.in/Notice/Academic%20Leadership%20-%202017.pdf'
-    },
-    {
-        "title":"Degree Certificate Distribution Ceremony",
-        "url":"https://www.tcetmumbai.in/Notice/DCDCReport2019.pdf"
-    },
-    {
-        "title":"Multicon-W ",
-        "url":"https://multiconw.co.in/"
-    },
-    {
-        "title":"Zephyr",
-        "url":"https://www.tcetmumbai.in/Notice/Zephyr_Report.pdf"
-    },
-    {
-        "title":"मातृभाषा दिवस",
-        "url":"https://www.tcetmumbai.in/Notice/MatribhashaDivasReport.pdf"
-    },
-    {
-        "title":"Women's day",
-        "url":"https://www.tcetmumbai.in/Notice/Women's%20Day%20Report.pdf"
-    },
-    {
-        "title":"Teacher's Day ",
-        "url":"https://www.tcetmumbai.in/Notice/Teachers'%20Day%20report.pdf"
-    },
-    {
-        "title":"Alumni",
-        "url":"/"
-    },
-    {
-        "title":"GUSTO-Online Cultural Event",
-        "url":"https://www.tcetmumbai.in/Notice/GUSTO.pdf"
-    },
-    {
-        "title":"Sojourn ",
-        "url":"/"
-    },
-    {
-        "title":"STTP ",
-        "url":"https://www.tcetmumbai.in/Notice/STTP%20Report.pdf"
-    },
+  {
+      "title":"Academic Leadership",
+      "url":'https://www.tcetmumbai.in/Notice/Academic%20Leadership%20-%202017.pdf'
+  },
+  {
+      "title":"Degree Certificate Distribution Ceremony",
+      "url":"https://www.tcetmumbai.in/Notice/DCDCReport2019.pdf"
+  },
+  {
+      "title":"Multicon-W ",
+      "url":"https://multiconw.co.in/"
+  },
+  {
+      "title":"Zephyr",
+      "url":"https://www.tcetmumbai.in/Notice/Zephyr_Report.pdf"
+  },
+  {
+      "title":"मातृभाषा दिवस",
+      "url":"https://www.tcetmumbai.in/Notice/MatribhashaDivasReport.pdf"
+  },
+  {
+      "title":"Women's day",
+      "url":"https://www.tcetmumbai.in/Notice/Women's%20Day%20Report.pdf"
+  },
+  {
+      "title":"Teacher's Day ",
+      "url":"https://www.tcetmumbai.in/Notice/Teachers'%20Day%20report.pdf"
+  },
+  {
+      "title":"Alumni",
+      "url":"/"
+  },
+  {
+      "title":"GUSTO-Online Cultural Event",
+      "url":"https://www.tcetmumbai.in/Notice/GUSTO.pdf"
+  },
+  {
+      "title":"Sojourn ",
+      "url":"/"
+  },
+  {
+      "title":"STTP ",
+      "url":"https://www.tcetmumbai.in/Notice/STTP%20Report.pdf"
+  },
 ];
 
 export const EducationalSurveyData = [
-    {
-        "title":"SUSTAINABILITY OF ENGINEERING EDUCATION",
-        "url":"https://www.tcetmumbai.in/Notice/SEE.pdf",
-    },
-    {
-        "title":"Student Satisfacation Survey",
-        "url":"https://www.tcetmumbai.in/Notice/SSSurvey.pdf",
-    },
+  {
+      "title":"SUSTAINABILITY OF ENGINEERING EDUCATION",
+      "url":"https://www.tcetmumbai.in/Notice/SEE.pdf",
+  },
+  {
+      "title":"Student Satisfacation Survey",
+      "url":"https://www.tcetmumbai.in/Notice/SSSurvey.pdf",
+  },
 ]
-=======
-export default data;
->>>>>>> 8539fc1266a9c90cb819714b3204c0667484178d
