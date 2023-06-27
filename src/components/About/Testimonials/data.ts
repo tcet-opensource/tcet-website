02e32f5de5aa999f48346b523be6852f85a4d9f3
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-06-27 16:58:09 +0530

    [REFACTOR] components restructured correctly

--- src/components/About/Testimonials/data.ts
+++ src/components/About/Testimonials/data.ts
@@ -1,4 +1,6 @@
-export const slideObjCorporate= [
+export interface Testimony {testimony: string, author: string, company: string}
+
+export const slideObjCorporate: Testimony[]  = [
     {
       testimony:
         "Very impressive work portrayed by students under guidance from faculty Very impressive work portrayed by students under guidance from faculty",
@@ -25,7 +27,7 @@ export const slideObjCorporate= [
     },
   ];
 
-export const slideObjExperts = [
+export const slideObjExperts: Testimony[] = [
     {
         testimony: "Through its support & valuable contributions, the incubation centre has helped us sustain & grow our business over past year. We would like to thank Dr. Kamal Shah & Dr. Deven Shah for their guidance. Also, we would like to thank Maithlee Madam, Ashish Sir, Bhupesh & Manish for their support & effort on running Incubation Centre Smoothly On daily Basis.",
         author: "Roopam Mishra",
@@ -48,7 +50,7 @@ export const slideObjExperts = [
     },
 ];
 
-export const slideObjFaculty = [
+export const slideObjFaculty: Testimony[] = [
     {
         testimony: "Through its support & valuable contributions, the incubation centre has helped us sustain & grow our business over past year. We would like to thank Dr. Kamal Shah & Dr. Deven Shah for their guidance. Also, we would like to thank Maithlee Madam, Ashish Sir, Bhupesh & Manish for their support & effort on running Incubation Centre Smoothly On daily Basis.",
         author: "Hemant P. Kasturiwale",
