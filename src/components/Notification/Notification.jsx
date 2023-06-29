ed4f6994f8abc9246669f2d1a64c4e730450244a
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-06-29 23:43:28 +0530

    Changes

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -43,7 +43,7 @@ const Notification = () => {
                 viewBox="0 0 24 24"
                 strokeWidth={2}
                 stroke="currentColor"
-                className="w-6 h-6"
+                className="w-6 h-6 hover:bg-gray-200 rounded-lg"
               >
                 <path
                   strokeLinecap="round"
@@ -55,21 +55,21 @@ const Notification = () => {
           </div>
           <div>
             {notification.map((item) => (
-              <div className="border-t px-6 flex flex-row py-3 min-h-fit justify-start border-[#E4E7EC] items-baseline">
-                <div className="min-h-[8px] min-w-[8px] max-h-[8px] max-w-[8px] rounded-full bg-blue-500 mr-4"></div>
+              <div className="border-t px-6 flex flex-row py-3 min-h-fit justify-start border-[#E4E7EC] items-baseline hover:bg-gray-200 hover:shadow-xl">
+                <div className="min-h-[8px] min-w-[8px] max-h-[8px] max-w-[8px] rounded-full bg-blue-500 mr-4 hover:bg-gray-500"></div>
                 <a href={item.link}>
-                  <div className="text-sm ">{item.message}</div>
+                  <div className="text-sm">{item.message}</div>
                 </a>
               </div>
             ))}
           </div>
         </div>
       ) : (
-        <div className="fixed top-56 z-10 right-0 mx-8 rounded-lg">
+        <div className="fixed top-56 z-10 right-0 mx-8 rounded-full">
           <div className="px-3">
             <button
               onClick={onClickOpen}
-              className="bg-white p-3 rounded-full hover:drop-shadow-xl"
+              className="bg-white p-3 rounded-full shadow-xl hover:drop-shadow-xl hover:shadow-2xl hover:bg-gray-200"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
